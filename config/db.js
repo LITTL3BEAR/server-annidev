137769a942ec7f5353a3c236f53bab3048e98507
annisabd <annisabd@ais.co.th>
2024-01-01 05:06:46 +0700

    -

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('MongoDB connected.');
+    console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB', err.message);
     process.exit(1);
   }
 };
