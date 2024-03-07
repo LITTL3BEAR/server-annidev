a9205d2bdc12de8bc5c76ccc90f46664f86ad0f6
annisabd <annisabd@ais.co.th>
2024-03-07 08:13:28 +0700

    -

--- scripts/sync-manga.py
+++ scripts/sync-manga.py
@@ -5,11 +5,17 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 def scrape_web(url):
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, 'html.parser')
-  element = soup.find_all('div', class_='eph-num')
-  chapter = re.findall(r'\d+', element[1].span.text)[0]
-  return chapter
+  with requests.Session() as session:
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = session.get(url, headers=headers ,timeout=10)
+    if response.status_code == 200:
+      soup = BeautifulSoup(response.text, 'html.parser')
+      elements = soup.find_all('div', class_='eph-num')
+      if elements and len(elements) > 1:
+        chapter = re.findall(r'\d+', elements[1].span.text)[0]
+        return chapter
+  return None
+
 
 def update_db(manga):
   filter = { "_id": manga['_id'] }
@@ -17,19 +23,20 @@ def update_db(manga):
   result = collection.update_one(filter, update)
   return result
 
+
 client = MongoClient(sys.argv[1])
 db = client['annidev_db']
 collection = db['mangas']
 
 for manga in collection.find():
   if not manga['link']: continue
-
   chapter = scrape_web(manga['link'])
-  if not chapter: continue
+  print(f"{manga['name']}")
+#   if not chapter: continue
 
-  manga['status'] = "new" if int(chapter) > int(manga['chapter']) else "read"
-  update_db(manga)
-  # print(f"{manga['name']}|{chapter}|{manga['status']}")
+#   manga['status'] = "new" if int(chapter) > int(manga['chapter']) else "read"
+#   update_db(manga)
+#   # print(f"{manga['name']}|{chapter}|{manga['status']}")
 
 print(f"Sync Done")
 client.close()
